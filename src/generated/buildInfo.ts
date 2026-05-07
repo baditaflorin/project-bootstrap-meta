ee5dcddb3882a04bfd83dd06bde9110aacfc4504
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 02:11:20 +0300

    ops: publish pages base-path test fix

--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
   "version": "0.1.0",
-  "commit": "aeac3b9276f2",
-  "fullCommit": "aeac3b9276f2b1d3e7fb607df4c477bd9463ffce",
-  "sourceCommitSubject": "fix: use free strict smoke port",
-  "builtAt": "2026-05-07T23:03:22.976Z",
+  "commit": "1fffed06b8dd",
+  "fullCommit": "1fffed06b8dd09ba90400b9c4ca1daa72d2edd5d",
+  "sourceCommitSubject": "test: respect pages base path",
+  "builtAt": "2026-05-07T23:10:21.428Z",
   "repositoryUrl": "https://github.com/baditaflorin/project-bootstrap-meta",
   "paypalUrl": "https://www.paypal.com/paypalme/florinbadita",
   "liveUrl": "https://baditaflorin.github.io/project-bootstrap-meta/"
