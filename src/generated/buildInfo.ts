290b09aaf4f24d1e0a33e702e8e36d7c4a98fb9f
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 01:58:31 +0300

    ops: publish reproducible pages build

--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,9 +1,9 @@
 export const buildInfo = {
   "version": "0.1.0",
-  "commit": "d55dd9e6f47d",
-  "fullCommit": "d55dd9e6f47d8bdbf5e0be9e15c9303ddabdeadd",
-  "sourceCommitSubject": "fix: stabilize build metadata commit",
-  "builtAt": "2026-05-07T22:54:01.816Z",
+  "commit": "f568baedcce8",
+  "fullCommit": "f568baedcce8d7295a2704fc5a3961a4dfbc7c30",
+  "sourceCommitSubject": "fix: make build metadata reproducible",
+  "builtAt": "2026-05-07T22:57:50.406Z",
   "repositoryUrl": "https://github.com/baditaflorin/project-bootstrap-meta",
   "paypalUrl": "https://www.paypal.com/paypalme/florinbadita",
   "liveUrl": "https://baditaflorin.github.io/project-bootstrap-meta/"
