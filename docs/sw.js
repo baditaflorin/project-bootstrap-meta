d6d1d2a2735280dba22569c57b09ada0a1636a33
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 01:40:14 +0300

    ops: publish pages build

---
+++ docs/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'project-bootstrap-meta-v1';
+const APP_SHELL = ['/project-bootstrap-meta/', '/project-bootstrap-meta/site.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+        }
+        return response;
+      });
+    })
+  );
+});
--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,8 +1,8 @@
 export const buildInfo = {
   "version": "0.1.0",
-  "commit": "03c027460c74",
-  "fullCommit": "03c027460c74acf5acf2472de69bdbccc0f98975",
-  "builtAt": "2026-05-07T22:38:41.898Z",
+  "commit": "0b3cde23368d",
+  "fullCommit": "0b3cde23368d93b4272150042890a8a97aa0b8e8",
+  "builtAt": "2026-05-07T22:40:00.007Z",
   "repositoryUrl": "https://github.com/baditaflorin/project-bootstrap-meta",
   "paypalUrl": "https://www.paypal.com/paypalme/florinbadita",
   "liveUrl": "https://baditaflorin.github.io/project-bootstrap-meta/"
